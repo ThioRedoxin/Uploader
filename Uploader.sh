99df44845d29ee6e74602be7c00eb0b7aecd1be1
Thio.Redoxin@gmx.de <Thio.Redoxin@gmx.de>
2019-08-15 18:26:38 +0200

    new update

--- Uploader.sh
+++ Uploader.sh
@@ -6,6 +6,6 @@ cp /home/sysgen/Documents/LWB/PredictingProteinInteractions/data/106Test/NNexper
 
 git add .
 git -c "user.name=Thio.Redoxin@gmx.de" -c "user.email=Thio.Redoxin@gmx.de" commit -m "new update"
-git push origin master
+git -c "user.name=Thio.Redoxin@gmx.de" -c "user.email=Thio.Redoxin@gmx.de" push origin master
 
 echo "uploaded to github"
