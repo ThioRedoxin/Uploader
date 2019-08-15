54fec7aa9dfa26672a8d7fb439a197a7903365dc
Thio.Redoxin@gmx.de <Thio.Redoxin@gmx.de>
2019-08-15 18:28:05 +0200

    new update

--- Uploader.sh
+++ Uploader.sh
@@ -1,11 +1,14 @@
  #!/bin/sh
  
+git config user.name "Thio.Redoxin@gmx.de"
+git config user.email "Thio.Redoxin@gmx.de"
+ 
  echo "hi"
  
 cp /home/sysgen/Documents/LWB/PredictingProteinInteractions/data/106Test/NNexperimentsKfoldCV/summary.csv summary.csv
 
 git add .
-git -c "user.name=Thio.Redoxin@gmx.de" -c "user.email=Thio.Redoxin@gmx.de" commit -m "new update"
-git -c "user.name=Thio.Redoxin@gmx.de" -c "user.email=Thio.Redoxin@gmx.de" push origin master
+git commit -m "new update"
+git push origin master
 
 echo "uploaded to github"
